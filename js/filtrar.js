60a6339439b4698a93a8024472cdfc54f5008aaa
Jacó Apolinário <jacoapollinario@gmail.com>
2019-08-16 15:49:09 -0300

    Novas funcionalidade adicionadas

--- js/filtrar.js
+++ js/filtrar.js
@@ -3,9 +3,22 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 campoFiltro.addEventListener("input", function() {
   console.log(this.value);
   var pacientes = document.querySelectorAll(".paciente");
-  for (var posicao = 0; posicao < pacientes.length; posicao++) {
-    var paciente = pacientes[posicao];
-    var tdNome = paciente.querySelector(".info-nome");
-    var nome = paciente.textContent;
+
+  if (this.value.length > 0) {
+    for (var posicao = 0; posicao < pacientes.length; posicao++) {
+      var paciente = pacientes[posicao];
+      var tdNome = paciente.querySelector(".info-nome");
+      var nome = tdNome.textContent;
+      if (nome != this.value) {
+        paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+      }
+    }
+  } else {
+    for (var posicao = 0; posicao < pacientes.length; posicao++) {
+      var paciente = pacientes[posicao];
+      paciente.classList.remove("invisivel");
+    }
   }
 });
